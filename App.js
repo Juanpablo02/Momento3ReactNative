9617f0960702a43a07ce5a6a1c234ec5b2373811
203 <203@academicos.cesde.edu.co>
2022-11-09 07:19:26 -0500

    Se crea aplicacion con las 2 pantallas

--- App.js
+++ App.js
@@ -1,12 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  Button,
+  View,
+  TextInput,
+  Touchable,
+  TouchableOpacity,
+  Image
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useState } from "react";
+import { useForm, Controller } from 'react-hook-form';
+import { Ionicons } from "@expo/vector-icons";
+
+
+function Vendedor({ route }) {
+  return (
+    <View>
+      <Text>Inicio</Text>
+    </View>
+  );
+}
+
+function Venta({ route }) {
+  return (
+    <View>
+      <Text>Inicioo</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Tab.Screen
+        name="Vendedor"
+        component={Vendedor}
+        options={{
+          
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-home" size={20} color={color} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Venta"
+        component={Venta}
+        options={{ title: "Venta",
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="person" size={20} color={color} />
+        ) }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeTabs}
+          options={{ title: "Sistema de Ventas" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
