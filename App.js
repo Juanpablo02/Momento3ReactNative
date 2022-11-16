9e9f8fb9a0ce2b2bc3aa4fe5776e316585f81405
203 <203@academicos.cesde.edu.co>
2022-11-16 08:05:04 -0500

    Se crearon las funciones de guardar y buscar vendedores

--- App.js
+++ App.js
@@ -7,7 +7,8 @@ import {
   TextInput,
   Touchable,
   TouchableOpacity,
-  Image
+  Image,
+  ActivityIndicator
 } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -18,12 +19,135 @@ import { Ionicons } from "@expo/vector-icons";
 
 
 function Vendedor({ route }) {
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [idsearch, setIdSearch] = useState("");
+  const [username, setUsername] = useState("")
+  const [nombre, setNombre] = useState("");
+  const [apellidos, setApellidos] = useState("")
+  const [correo, setCorreo] = useState("")
+  const [comision, setComision] = useState("")
+
+const getVendedorById = async (id) => {
+  try{
+    const url = `http://172.16.63.73:3000/api/clientes/${id}`;
+    const response = await axios.get(url);
+    setData(response.data.nombre)
+    setData(response.data.correo)
+    setData(response.data.comision)
+    setData(response.data.idsearch)
+    //setNombre(response.data.nombre);
+    //setApellidos(response.data.apellidos);
+  }
+  catch(error){
+    console.log(error)
+  }
+  finally{
+    setLoading(false)
+  }
+};
+
+const saveVendendor = async () => {
+  if (!nombre.trim() || !correo.trim() || !comision.trim()) {
+    alert("Nombre y usuario inválido");
+    return;
+  }
+  setLoading(true);
+  try {
+    const response = await axios.post(`http://172.16.63.73:3000/api/clientes`, {
+      nombre,
+      correo,
+      comision
+    });
+    alert("Vendedor agregado correctamente ...")
+    setNombre("")
+    setCorreo("")
+    setComision("")
+    getClientes()
+  } catch (error) {
+    console.log(error)
+  }
+  finally{
+    setLoading(false);
+  }
+};
+
+const {control,handleSubmit,formState: { errors }} = useForm(
+  {
+    defaultValues: {
+      nombre: nombre,
+      correo: correo,
+      comision: comision
+    }
+  }
+);
+
   return (
-    <View>
-      <Text>Inicio</Text>
+    <View style={{ flex: 1, padding: 24 }}>
+      <View>
+        <Text style={{marginBottom:10,textAlign:"center",fontSize:20}}>Vendendores</Text>
+
+        <TextInput
+          style={[styles.input]}
+          placeholder="Ingrese le id del vendedor"
+          onChangeText={idsearch => setIdSearch(idsearch)}
+          value={idsearch}/>
+
+          <TextInput
+          style={[styles.input]}
+          value={nombre}
+          placeholder="Nombre"
+          onChangeText={nombre => setNombre(nombre)}/>
+
+          <TextInput
+          style={[styles.input]}
+          value={correo}
+          placeholder="Correo Electronico"
+          onChangeText={correo => setCorreo(correo)}/>
+
+          <TextInput
+          style={[styles.input]}
+          value={comision}
+          placeholder="Comision"
+          onChangeText={comision => setComision(comision)}/>
+
+          <TouchableOpacity
+          style={[styles.buttons,{backgroundColor:"#7EFF78",marginBottom:15}]}
+          onPress={() => saveVendendor()}>
+        <Text>Guardar Vendedor</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+      style={[styles.buttons,{backgroundColor:"#5DCBFF",marginBottom:15}]}
+      onPress={getVendedorById}>
+        <Text>Buscar Vendedor</Text>
+      </TouchableOpacity>
+      </View>
+      {isLoading ? <ActivityIndicator size="large" color="#0000ff" /> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <View style={styles.boxUsers}>
+              <Text>Id Cliente: {item._id}</Text>
+              <Text>Nombre: {item.nombre}</Text>
+              <Text>Apellidos: {item.apellidos}</Text>
+              <TouchableOpacity
+                style={[styles.buttons,{backgroundColor:"#00000050"}]}
+                onPress = {() => {
+                  alert(item.nombre)
+                }
+              }>
+                <Text>Ver información</Text>
+              </TouchableOpacity>
+            </View>
+            
+          )}
+        />
+      )} 
     </View>
-  );
-}
+  )}
+  
 
 function Venta({ route }) {
   return (
@@ -86,4 +210,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  boxUsers: {
+    backgroundColor:"#00000020",borderRadius:15,marginBottom:15,padding:10
+  },
+  buttons:{
+    borderRadius:15,paddingVertical:5,textAlign:"center"
+  },
+  input:{
+    marginBottom:15,
+    backgroundColor:"#00000030",
+    paddingHorizontal:10,
+    paddingVertical:5,
+    borderRadius:15,
+    borderColor:"#00000040",
+    borderWidth:1
+  }
 });
